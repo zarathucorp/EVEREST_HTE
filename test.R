d04ab3539a88195ff106743c51f484c9c00eaa8f
Jinhwan Kim <hwanistic@gmail.com>
2023-02-24 13:58:15 +0900

    add example data

---
+++ test.R
@@ -0,0 +1,28 @@
+#' Test data set
+age 61
+sex 0
+
+bmi 25.3
+hb 17.47
+wbc 9.89
+
+dys 1
+htn 1
+smoking 0
+
+dia 0
+dia.i 0
+
+prior 0 0 0
+
+nstemi 1
+
+mvd.yn 1 
+pci.lm.yn 0
+mvd.pci.yn 1
+
+stent.no 1
+stent.lesion.no 1
+stent.length 22
+
+Result Score: -0.008773687
